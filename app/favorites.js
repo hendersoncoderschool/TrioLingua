57c81c7b72712bf22b21d38dd139afec79a91e19
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2023-07-21 09:56:26 -0700

    upload

---
+++ app/favorites.js
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export default function favorites() {
+  return (
+    <main>
+      <h1>Favorites</h1>
+     
+      <nav>
+        <a href = "#"><i class="fa fa-check-square"></i></a>
+        <a href = "#"><i class='fas fa-bell'></i></a>
+        <a href = "#"><i class='far fa-calendar-alt'></i></a>
+        <a href = "#"><i class='fas fa-wifi'></i></a>
+        <a href = "#"><i class='fas fa-star'></i></a>
+        <a href = "#"><i class='fas fa-heart'></i></a>
+      </nav>
+    </main>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import './globals.css'
 
-export default function Home() {
+export default function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <h1>TrioLingua</h1>
+      <div id = "spacing"></div>
+      <div class = "parent">
+        <div class = "child">
+      <img src = "bigger_dolphin.png"></img>
         </div>
+      <div class = "child card">
+        <h3>Welcome to TrioLingua, the perfect app to learn a language with a friend (or two)! Here at TrioLingua we believe that learning is best with a bit of fun, so we provide multiple competitive and cooperative games to play with your friends! And of course, we offer individual activities, best used to practice for some friendly competition.</h3>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+      <nav>
+        <a href = "#"><i class="fa fa-check-square"></i></a>
+        <a href = "#"><i class='fas fa-bell'></i></a>
+        <a href = "#"><i class='far fa-calendar-alt'></i></a>
+        <a href = "#"><i class='fas fa-wifi'></i></a>
+        <a href = "#"><i class='fas fa-star'></i></a>
+        <a href = "/favorites"><i class='fas fa-heart'></i></a>
+        <a href = "#"><i class='fa fa-shopping-cart'></i>
+</a>
+        
+      </nav>
     </main>
   )
 }
